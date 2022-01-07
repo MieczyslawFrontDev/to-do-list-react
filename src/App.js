1ccb787c62c6db2635af0681ab0e185aff8c53a9
frontend.dev.pm@gmail.com <frontend.dev.pm@gmail.com>
2022-01-07 09:57:36 +0100

    created localStorage.added useEffect and getInitialTasks function

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Container from "./Container";
 import Header from "./Header";
 import Section from "./Section";
@@ -7,15 +7,23 @@ import Tasks from "./Tasks";
 import Buttons from "./Buttons";
 import "./index.css";
 
+const getInitialTasks = () => {
+  const tasksFromLocaleStorage = localStorage.getItem("tasks");
+
+  return tasksFromLocaleStorage ? JSON.parse(tasksFromLocaleStorage) : []
+}
+
 function App() {
   const [hideDone, setHideDone] = useState(false);
-  const [tasks, setTasks] = useState([
-    { id: 1, content: "Zrobić szkolenie YouCode", done: false },
-    { id: 2, content: "Zostać programistą JS", done: false },
-  ]);
+
+  const [tasks, setTasks] = useState(getInitialTasks);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
 
   const toggleHideDone = () => {
-    setHideDone(hideDone => !hideDone);
+    setHideDone((hideDone) => !hideDone);
   };
 
   const removeTask = (id) => {
@@ -62,10 +70,7 @@ function App() {
       <>
         <Section
           title="Dodaj nowe zadanie"
-          body={
-            <Form 
-              title="Dodaj zadanie" 
-              addNewTask={addNewTask} />}
+          body={<Form addNewTask={addNewTask} />}
         />
         <Section
           title="Lista zadań"
