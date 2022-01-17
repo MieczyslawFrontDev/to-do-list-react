47133eb68ff549ae0ecd320dfb1e76c85f1eb9eb
frontend.dev.pm@gmail.com <frontend.dev.pm@gmail.com>
2022-01-18 00:14:36 +0100

    created hook useLocaleStorageState

---
+++ src/App/useLocaleStorageState.js
@@ -0,0 +1,17 @@
+import { useState, useEffect } from "react";
+
+export const useLocaleStorageState = () => {
+  const getInitialTasks = () => {
+    const tasksFromLocaleStorage = localStorage.getItem("tasks");
+
+    return tasksFromLocaleStorage ? JSON.parse(tasksFromLocaleStorage) : [];
+  };
+
+  const [tasks, setTasks] = useState(getInitialTasks);
+
+  useEffect(() => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  }, [tasks]);
+
+  return [tasks, setTasks]
+};
