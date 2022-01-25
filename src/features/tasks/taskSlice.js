20c7e82f68d3c613c66e8d2061222afa2f183d06
frontend.dev.pm@gmail.com <frontend.dev.pm@gmail.com>
2022-01-25 09:45:44 +0100

    created new files using redux

---
+++ src/features/tasks/taskSlice.js
@@ -0,0 +1,44 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const tasksSlice = createSlice({
+  name: "tasks",
+  initialState: {
+    tasks: [],
+    hideDone: false,
+  },
+
+  reducers: {
+    addTask: ({ tasks }, { payload: newTask }) => {
+      tasks.push(newTask);
+    },
+    toggleHideDone: (state) => {
+      state.hideDone = !state.hideDone;
+    },
+    toggleTaskDone: ({ tasks }, { payload: taskId }) => {
+      const index = tasks.findIndex((task) => task.id === taskId);
+      tasks[index].done = !tasks[index].done;
+    },
+    removeTask: ({ tasks }, { payload: taskId }) => {
+      const index = tasks.findIndex((task) => task.id === taskId);
+      tasks.splice(index, 1);
+    },
+    setAllDone: ({ tasks }) => {
+      tasks.map((task) => (task.done = true));
+    },
+    isEveryTaskDone: ({tasks}) => {
+      tasks.every(({ done }) => done)
+    }
+  },
+});
+
+export const {
+  addTask,
+  toggleHideDone,
+  toggleTaskDone,
+  removeTask,
+  setAllDone,
+  isEveryTaskDone
+} = tasksSlice.actions;
+
+export const selectTasks = (state) => state.tasks;
+export default tasksSlice.reducer;
