5c12524f4cdc8f4d738c41e6710686d6bfc699aa
frontend.dev.pm@gmail.com <frontend.dev.pm@gmail.com>
2022-01-25 09:43:45 +0100

    refactor to redux

--- src/features/tasks/Tasks.js
+++ src/features/tasks/Tasks.js
@@ -1,53 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 import Wrapper from "../../common/Container";
 import Header from "../../common/Header";
 import Section from "../../common/Section";
 import Form from "./Form";
 import TaskList from "./TaskList";
 import Buttons from "./Buttons";
-import { useTasks} from "../../useTasks";
 
 function Tasks() {
-
-  const [hideDone, setHideDone] = useState(false);
-
-  const toggleHideDone = () => {
-    setHideDone((hideDone) => !hideDone);
-  };
-
-  const {
-    tasks,
-    removeTask,
-    toggleTaskDone,
-    setAllDone,
-    addNewTask
-  } = useTasks();
-
   return (
     <Wrapper>
-      <Header title="Lista zadań" />
+      <Header />
       <>
-        <Section
-          title="Dodaj nowe zadanie"
-          body={<Form addNewTask={addNewTask} />}
-        />
-        <Section
-          headerContent={
-            <Buttons
-              tasks={tasks}
-              hideDone={hideDone}
-              toggleHideDone={toggleHideDone}
-              setAllDone={setAllDone}
-            />
-          }
-          body={
-            <TaskList
-              tasks={tasks}
-              hideDone={hideDone}
-              removeTask={removeTask}
-              toggleTaskDone={toggleTaskDone}
-            />
-          }
+        <Section body={<Form />} />
+        <Section 
+          headerContent={<Buttons />} 
+          body={<TaskList />} 
         />
       </>
     </Wrapper>
