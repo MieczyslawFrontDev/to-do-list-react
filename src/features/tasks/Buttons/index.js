f90450e2fb089c848e3911bbee62cbb1777eddce
frontend.dev.pm@gmail.com <frontend.dev.pm@gmail.com>
2022-01-25 09:47:04 +0100

    imported and used useSelector and useDispatch in component

--- src/features/tasks/Buttons/index.js
+++ src/features/tasks/Buttons/index.js
@@ -1,22 +1,34 @@
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  selectTasks,
+  toggleHideDone,
+  setAllDone,
+  isEveryTaskDone
+} from "../taskSlice";
 import { Wrapper, Button } from "./styled";
 
-const Buttons = ({ tasks, hideDone, toggleHideDone, setAllDone }) => (
-  <Wrapper>
-    {tasks.length > 0 && (
-      <>
-        <Button onClick={toggleHideDone}>
-          {hideDone ? "Pokaż" : "Ukryj"} ukończone
-        </Button>
-        <Button
-          disabled={tasks.every(({ done }) => done)}
-          onClick={setAllDone}
-        >
-          Ukończ wszystkie
-        </Button>
-      </>
-    )}
-  </Wrapper>
-);
+const Buttons = () => {
+  const { tasks, hideDone } = useSelector(selectTasks);
+  const dispatch = useDispatch();
+
+  return (
+    <Wrapper>
+      {tasks.length > 0 && (
+        <>
+          <Button onClick={() => dispatch(toggleHideDone())}>
+            {hideDone ? "Pokaż" : "Ukryj"} ukończone
+          </Button>
+          <Button
+            disabled={isEveryTaskDone}
+            onClick={() => dispatch(setAllDone())}
+          >
+            Ukończ wszystkie
+          </Button>
+        </>
+      )}
+    </Wrapper>
+  );
+};
 
 export default Buttons;
