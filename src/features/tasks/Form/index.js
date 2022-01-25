8426c5f71e243ed7cef0849f5f22ab9fd2e6f252
frontend.dev.pm@gmail.com <frontend.dev.pm@gmail.com>
2022-01-25 09:51:05 +0100

    used Dispatch hook in component

--- src/features/tasks/Form/index.js
+++ src/features/tasks/Form/index.js
@@ -1,9 +1,14 @@
 import { useState, useRef } from "react";
 import { StyledForm, Input, Button } from "./styled";
+import { useDispatch } from "react-redux";
+import { addTask } from "../taskSlice";
+import { nanoid } from "@reduxjs/toolkit";
 
-const Form = ({ addNewTask }) => {
+const Form = () => {
   const [newTaskContent, setNewTaskContent] = useState("");
   const inputRef = useRef(null);
+
+const dispatch = useDispatch()
 
   const onFormSubmit = (event) => {
     event.preventDefault();
@@ -13,7 +18,11 @@ const Form = ({ addNewTask }) => {
     if (!trimmedNewTaskContent) {
       return;
     }
-    addNewTask(trimmedNewTaskContent);
+    dispatch(addTask({
+      content: trimmedNewTaskContent,
+      done: false,
+      id: nanoid(),
+    }))
     setNewTaskContent("");
     inputRef.current.focus();
   };
