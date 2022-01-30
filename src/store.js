c86a6a30695bb6410d85f0053669d45719241bc9
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-01-30 12:42:41 +0100

    created store

--- src/store.js
+++ src/store.js
@@ -1,8 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
 import tasksReducer from "./features/tasks/taskSlice";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./rootSaga";
+
+const sagaMiddleware = createSagaMiddleware();
 
 export default configureStore({
     reducer: {
         tasks: tasksReducer,
-    }
+    },
+    middleware: [sagaMiddleware],
 })
+
+sagaMiddleware.run(rootSaga);
