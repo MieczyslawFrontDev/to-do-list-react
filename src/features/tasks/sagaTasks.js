0b96c66f0d4eb3bfce44ead5d9e5532d12cd40cb
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-01-30 12:45:40 +0100

    used redux-saga

---
+++ src/features/tasks/sagaTasks.js
@@ -0,0 +1,24 @@
+import { call, put, select, takeEvery, takeLatest } from "redux-saga/effects";
+import { getExampleTasks } from "./getExampleTasks";
+import { saveTasksInLocaleStorage } from "./tasksFromLocaleStorage";
+import { axiosExampleTasks, selectTasks, setTasks } from "./taskSlice";
+
+function* axiosExampleTasksWorker() {
+  try {
+    const exampleTasks = yield call(getExampleTasks);
+    yield put(setTasks(exampleTasks));
+  } catch (error) {
+    yield call(error, alert, "coś poszło nie tak");
+  }
+}
+
+function* saveTasksInLocaleStorageWorker(){
+    const tasks = yield select(selectTasks);
+    yield call(saveTasksInLocaleStorage, tasks)
+}
+
+
+export function* tasksSaga() {
+  yield takeLatest(axiosExampleTasks.type, axiosExampleTasksWorker);
+  yield takeEvery("*", saveTasksInLocaleStorageWorker)
+}
