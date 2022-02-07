f2c3cfcd4f638ed8f8fc6ef32767f71619dfb1d7
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-02-07 15:02:04 +0100

    created TasksPage component

---
+++ src/features/tasks/TasksPage/index.js
@@ -0,0 +1,37 @@
+import React from "react";
+import Button from "../../../common/Button";
+import Wrapper from "../../../common/Container";
+import Header from "../../../common/Header";
+import Section from "../../../common/Section";
+import Form from "../Form";
+import TaskList from "../TaskList";
+import Buttons from "../Buttons";
+import Search from "../Search";
+
+
+function TasksPage() {
+
+  return (
+    <Wrapper>
+      <Header title="Lista zadań" />
+      <>
+        <Section
+          title="Dodaj nowe zadanie"
+          button={<Button />}
+          body={<Form />} 
+        />
+        <Section
+          title="Dodaj nowe zadanie" 
+          body={<Search />} 
+        />
+        <Section 
+          title="Lista zadań"
+          headerContent={<Buttons />} 
+          body={<TaskList />} 
+        />
+      </>
+    </Wrapper>
+  );
+}
+
+export default TasksPage;
