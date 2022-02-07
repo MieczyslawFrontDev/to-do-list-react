ec80e2cb333e81da2997ea15b584f4b17b3ef464
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-02-07 14:26:39 +0100

    Used react router dom and styled components added

--- src/App.js
+++ src/App.js
@@ -1,4 +1,36 @@
 import React from "react";
-import Tasks from "./features/tasks/Tasks";
+import { HashRouter, Redirect, Route, Switch } from "react-router-dom";
 
-export const App = () => <Tasks />;
+import TasksPage from "./features/tasks/TasksPage";
+import TaskPage from "./features/tasks/TaskPage";
+import { Author } from "./features/author";
+import { Navigation, List, StyledNavLink, Item } from "./AppStyled"
+
+export const App = () => (
+  <HashRouter>
+    <Navigation>
+      <List>
+        <Item>
+          <StyledNavLink to="/zadania">Zadania</StyledNavLink>
+        </Item>
+        <Item>
+          <StyledNavLink to="/autor">O autorze</StyledNavLink>
+        </Item>
+      </List>
+    </Navigation>
+    <Switch>
+      <Route path="/zadania/:id">
+        <TaskPage />
+      </Route>
+      <Route path="/zadania">
+        <TasksPage />
+      </Route>
+      <Route path="/autor">
+        <Author />
+      </Route>
+      <Route path="/">
+        <Redirect to="/zadania"/>
+      </Route>
+    </Switch>
+  </HashRouter>
+);
---
+++ src/AppStyled.js
@@ -0,0 +1,25 @@
+import styled from "styled-components";
+import { NavLink } from "react-router-dom";
+
+export const StyledNavLink = styled(NavLink)`
+color: white;
+text-decoration: none;
+letter-spacing: 1px;
+
+&.active {
+    font-weight: bold;
+}
+`
+export const Navigation = styled.nav`
+background-color: teal;
+`
+export const List = styled.ul`
+display: flex;
+justify-content: center;
+margin: 0;
+padding: 0;
+`
+export const Item = styled.li`
+list-style: none;
+margin: 20px;
+`
