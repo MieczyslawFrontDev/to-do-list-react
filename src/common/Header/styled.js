245f5f6926fc89ac871a989414f676915a4f6b87
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-02-07 14:34:23 +0100

    Changed margin and added media

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -1,5 +1,9 @@
 import styled from "styled-components";
 
 export const Title = styled.h1`
-  margin-bottom: 0;
+  margin: 0;
+
+  @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
+    text-align: center;
+  }
 `;
