c3f5fdc4b71db84e12d964264cf596c9c0698aaf
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-02-07 14:45:48 +0100

    created new file with localestorage

--- src/features/tasks/tasksFromLocaleStorage.js
+++ src/features/tasks/tasksFromLocaleStorage.js
@@ -2,7 +2,6 @@ const localStorageKey = "tasks";
 
 export const saveTasksInLocaleStorage = (tasks) =>
   localStorage.setItem(localStorageKey, JSON.stringify(tasks));
-console.log(saveTasksInLocaleStorage);
 
 export const getTasksFromLocaleStorage = () =>
   JSON.parse(localStorage.getItem(localStorageKey)) || [];
