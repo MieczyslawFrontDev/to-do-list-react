03f1c70cead93d9ce102d19823acc4b7456e3e7b
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-12-07 12:06:00 +0100

    changed align items to justify-self property in button component

--- src/features/tasks/TaskList/index.js
+++ src/features/tasks/TaskList/index.js
@@ -1,6 +1,11 @@
 import { useSelector, useDispatch } from "react-redux";
 import { List, Item, Button, Content, StyledLink } from "./styled";
-import { toggleTaskDone, removeTask, selectHideDone, selectTasksByQuery} from "../taskSlice";
+import {
+  toggleTaskDone,
+  removeTask,
+  selectHideDone,
+  selectTasksByQuery,
+} from "../taskSlice";
 import { useLocation } from "react-router-dom";
 import searchQueryParamName from "../searchQueryParamName";
 
@@ -9,28 +14,21 @@ const TaskList = () => {
   const searchParams = new URLSearchParams(location.search);
   const query = searchParams.get(searchQueryParamName);
 
-  const tasks = useSelector(state => selectTasksByQuery(state, query));
+  const tasks = useSelector((state) => selectTasksByQuery(state, query));
   const hideDone = useSelector(selectHideDone);
   const dispatch = useDispatch();
 
   return (
     <List>
       {tasks.map((task) => (
-        <Item 
-          key={task.id} 
-          hidden={task.done && hideDone}>
-          <Button 
-            toggleDone 
-            onClick={() => dispatch(toggleTaskDone(task.id))}>
+        <Item key={task.id} hidden={task.done && hideDone}>
+          <Button toggleDone onClick={() => dispatch(toggleTaskDone(task.id))}>
             {task.done ? "✓" : ""}
           </Button>
           <Content done={task.done}>
-              <StyledLink to={`/zadania/${task.id}`}>{task.content}</StyledLink>
+            <StyledLink to={`/zadania/${task.id}`}>{task.content}</StyledLink>
           </Content>
-          <Button 
-            remove
-            onClick={() => dispatch(removeTask(task.id))}
-          >
+          <Button remove onClick={() => dispatch(removeTask(task.id))}>
             🗑
           </Button>
         </Item>
--- src/features/tasks/TaskList/styled.js
+++ src/features/tasks/TaskList/styled.js
@@ -1,5 +1,5 @@
 import styled, { css } from "styled-components";
-    import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 export const List = styled.ul`
   list-style: none;
@@ -13,13 +13,13 @@ export const List = styled.ul`
 `;
 
 export const Item = styled.li`
+  list-style-type: none;
   display: grid;
   grid-template-columns: auto 1fr auto;
   grid-column-gap: 10px;
   padding: 10px;
   border-bottom: 1px solid lightgray;
   align-items: center;
-
   ${({ hidden }) =>
     hidden &&
     css`
@@ -33,12 +33,12 @@ export const Button = styled.button`
   width: 30px;
   height: 30px;
   border-radius: 2px;
+  justify-self: center;
 
   ${({ toggleDone }) =>
     toggleDone &&
     css`
       background-color: ${({ theme }) => theme.color.green};
-      text-align: center;
 
       &:hover {
         transform: scale(1.1);
@@ -53,8 +53,6 @@ export const Button = styled.button`
     remove &&
     css`
       background-color: ${({ theme }) => theme.color.red};
-      text-align: center;
-
 
       &:hover {
         transform: scale(1.1);
@@ -76,5 +74,4 @@ export const Content = styled.span`
 export const StyledLink = styled(Link)`
   text-decoration: none;
   color: teal;
-  
-`
+`;
