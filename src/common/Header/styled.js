c85a891e4a0af78ff0d5335d1d1d9ac583920ecc
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-12-08 21:48:07 +0100

    added align-items property to Title

--- src/common/Header/styled.js
+++ src/common/Header/styled.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 
 export const Title = styled.h1`
   margin: 0;
+  text-align: center;
 
   @media (max-width: ${({ theme }) => theme.breakpoint.mobileMax}px) {
     text-align: center;
