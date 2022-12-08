962c8ad0bf3447f408c0b8d2aebd9fc1be4c2ceb
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-12-08 21:43:33 +0100

    added hover to Item component

--- src/AppStyled.js
+++ src/AppStyled.js
@@ -2,24 +2,29 @@ import styled from "styled-components";
 import { NavLink } from "react-router-dom";
 
 export const StyledNavLink = styled(NavLink)`
-color: white;
-text-decoration: none;
-letter-spacing: 1px;
+  color: white;
+  text-decoration: none;
+  letter-spacing: 1px;
 
-&.active {
+  &.active {
     font-weight: bold;
-}
-`
+  }
+`;
 export const Navigation = styled.nav`
-background-color: teal;
-`
+  background-color: teal;
+`;
 export const List = styled.ul`
-display: flex;
-justify-content: center;
-margin: 0;
-padding: 0;
-`
+  display: flex;
+  justify-content: center;
+  margin: 0;
+  padding: 0;
+`;
 export const Item = styled.li`
-list-style: none;
-margin: 20px;
-`
+  list-style: none;
+  margin: 20px;
+
+  &:hover {
+    transition: 0.5s;
+    transform: scale(1.1);
+  }
+`;
