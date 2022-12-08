cbafba11a0175681af9facedb596a6faacd7f261
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-12-08 21:43:57 +0100

    added productsSaga to rootSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,8 +1,7 @@
 import { all } from "redux-saga/effects";
+import { productsSaga } from "./features/shopping/productsSaga";
 import { tasksSaga } from "./features/tasks/TasksSaga";
 
-export function* rootSaga(){
-    yield all([
-        tasksSaga(),
-    ])
+export function* rootSaga() {
+  yield all([tasksSaga(), productsSaga()]);
 }
