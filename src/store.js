e4c97db2b349ace367648abbc1e5558bc16a9a25
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-12-08 21:45:30 +0100

    added products to reducer

--- src/store.js
+++ src/store.js
@@ -1,15 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
-import tasksReducer from "./features/tasks/taskSlice";
+import tasksReducer from "./features/tasks/tasksSlice";
+import productsReducer from "./features/shopping/productsSlice";
 import createSagaMiddleware from "redux-saga";
 import { rootSaga } from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 
 export default configureStore({
-    reducer: {
-        tasks: tasksReducer,
-    },
-    middleware: [sagaMiddleware],
-})
+  reducer: {
+    tasks: tasksReducer,
+    products: productsReducer,
+  },
+  middleware: [sagaMiddleware],
+});
 
 sagaMiddleware.run(rootSaga);
