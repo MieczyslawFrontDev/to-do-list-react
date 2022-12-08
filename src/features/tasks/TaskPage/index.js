399f62f1b2f7a599b8964d7c8aea7d0bd7be1558
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-12-08 22:09:41 +0100

    changed export TaskPage component, changed import getTaskById func

--- src/features/tasks/TaskPage/index.js
+++ src/features/tasks/TaskPage/index.js
@@ -4,24 +4,28 @@ import { useParams } from "react-router-dom";
 import { Wrapper } from "../../../common/Container/styled";
 import Header from "../../../common/Header";
 import Section from "../../../common/Section";
-import { getTaskById } from "../taskSlice";
+import { getTaskById } from "../tasksSlice";
 
-export default () => {
-const { id } = useParams();
-const task = useSelector(state => getTaskById(state, id))
+export const TaskPage = () => {
+  const { id } = useParams();
+  const task = useSelector((state) => getTaskById(state, id));
 
-    return (
-        <Wrapper>
-          <Header title={"Szczegóły zadania"} />
-            <Section
-              title={
-                task 
-                ? task.content 
-                : "Nie znaleziona zadania"}
-              body={task 
-                ? <><strong>Ukończone: </strong>{task.done ? "Tak" : "Nie"}</> 
-                : ""} 
-            />
-        </Wrapper>
-    )
-}
+  return (
+    <Wrapper>
+      <Header title={"Szczegóły zadania"} />
+      <Section
+        title={task ? task.content : "Nie znaleziona zadania"}
+        body={
+          task ? (
+            <>
+              <strong>Ukończone: </strong>
+              {task.done ? "Tak" : "Nie"}
+            </>
+          ) : (
+            ""
+          )
+        }
+      />
+    </Wrapper>
+  );
+};
