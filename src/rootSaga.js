17ee29a85f547ee67c84e99adb6ccff987d0714e
Mieczyslaw Przytula <frontend.dev.pm@gmail.com>
2022-12-08 21:57:22 +0100

    changed name TasksSaga to tasksSaga

--- src/rootSaga.js
+++ src/rootSaga.js
@@ -1,6 +1,6 @@
 import { all } from "redux-saga/effects";
 import { productsSaga } from "./features/shopping/productsSaga";
-import { tasksSaga } from "./features/tasks/TasksSaga";
+import { tasksSaga } from "./features/tasks/tasksSaga";
 
 export function* rootSaga() {
   yield all([tasksSaga(), productsSaga()]);
